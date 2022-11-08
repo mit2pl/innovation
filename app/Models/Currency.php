2b5e8e2c12b6f9469b3c6996a26cf1fd3086ae9f
mit2 <45004422+mit2pl@users.noreply.github.com>
2022-11-08 11:44:41 +0100

    create currency model

---
+++ app/Models/Currency.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'currency_code',
+        'exchange_rate'
+    ];
+    protected $casts = [
+        'created_at' => 'datetime:d/m/Y H:i:s',
+        'updated_at' => 'datetime:d/m/Y H:i:s',
+    ];
+}
